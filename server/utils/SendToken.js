2eb7706102423af273a5cc6ee968ff58144601ba
anish-stack <anishjha896@gmail.com>
2025-02-10 15:39:01 +0530

    add many details

--- server/utils/SendToken.js
+++ server/utils/SendToken.js
@@ -11,8 +11,8 @@ const sendToken = async (user, res, status, message) => {
             expires: new Date(
                 Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
             ),
-            httpOnly: true,
-            secure: true
+          
+        
         };
 
         // Send token in cookie
